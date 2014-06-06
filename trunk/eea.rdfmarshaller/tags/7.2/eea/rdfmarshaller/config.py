1d4d7f6e6cb50efdd11bc659cd22104cfe1916f8
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-06-06 13:44:00 +0000

    Tagged eea.rdfmarshaller 7.2.

---
+++ trunk/eea.rdfmarshaller/tags/7.2/eea/rdfmarshaller/config.py
@@ -0,0 +1,8 @@
+""" config module """
+
+import surf
+
+surf.ns.register(ATCT="http://plone.org/products/atcontenttypes#")
+surf.ns.register(ATCTNEWS="http://plone.org/products/atcontenttypes/News#")
+
+DEBUG = False
