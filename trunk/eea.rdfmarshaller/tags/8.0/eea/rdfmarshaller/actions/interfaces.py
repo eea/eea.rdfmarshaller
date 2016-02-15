c6806b3e6696839919ef16ca7c8829f7e5b21c28
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2016-02-15 12:30:24 +0000

    Tagged eea.rdfmarshaller 8.0.

---
+++ trunk/eea.rdfmarshaller/tags/8.0/eea/rdfmarshaller/actions/interfaces.py
@@ -0,0 +1,7 @@
+""" Custom event for Object moved or renamed """
+from zope.lifecycleevent.interfaces import IObjectMovedEvent
+
+class IObjectMovedOrRenamedEvent(IObjectMovedEvent):
+    """IObjectMovedOrRenamedEvent
+    """
+    
