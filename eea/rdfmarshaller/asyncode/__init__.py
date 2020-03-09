29694ec63d9084d3582fb7524cd2b6a33f3d1e02
David Ichim <ichim.david@gmail.com>
2020-03-09 19:50:34 +0200

    Refs #115533 commented plone.app.async import:
    - async is a reserved keyword starting with python 3.7 and
      attempting to do a try except catch didn't work, to be
      checked at the end of the migration on what can be done,
      ideally would be for plone.app.async to be renamed and
      for imports to be updated

--- eea/rdfmarshaller/asyncode/__init__.py
+++ eea/rdfmarshaller/asyncode/__init__.py
@@ -2,11 +2,16 @@
 """
 from zope.interface import Interface
 
-try:
-    from plone.app.async.interfaces import IAsyncService
-except ImportError:
-    class IAsyncService(Interface):
-        """ No async """
+# BBB Fix this as on python 3.7 even attempting to catch SyntaxError fails
+#try:
+#    from plone.app.async.interfaces import IAsyncService
+#except (ImportError, SyntaxError) as e:
+#    class IAsyncService(Interface):
+#        """ No async """
+
+
+class IAsyncService(Interface):
+    """ No async """
 
 __all__ = [
     IAsyncService.__name__,
